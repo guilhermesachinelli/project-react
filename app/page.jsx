20650d76dd95c509a80fe4992020c305fdcac05c
unknown <guilhermesachinelli.564@gamil.com>
2023-09-18 22:19:01 -0300

    feat: new desing main

--- app/page.jsx
+++ app/page.jsx
@@ -14,23 +14,36 @@ export default function Home() {
           </section>
         </div>
       </div>
-        <h2>Somos uma plataforma completa de</h2>
-        <h2> aprendizado em programação</h2>
-      <div className={styles.cardContainer}>
+      <h2>Somos uma plataforma completa de</h2>
+      <h2> aprendizado em programação</h2>
         <div className={styles.card}>
           <h1>Acessibilidade</h1>
           <p>Nossa plataforma foi projetada com o usuário em mente, tornando a programação acessível a todos, independentemente de seu nível de conhecimento prévio.</p>
-        </div>
-        <div className={styles.card}>
-          <h1>Variedade de Linguagens e Tecnologias</h1>
-          <p> Temos uma ampla gama de linguagens e tecnologias. Desde linguagens populares como Python, JavaScript e Java até frameworks e bibliotecas de última geração.</p>
-        </div>
-        <div className={styles.card}>
+          <hr />
           <h1>Aprendizado Interativo e Prático</h1>
           <p>Acreditamos que a melhor maneira de aprender programação é na prática. Oferecemos uma experiência de aprendizado, na qual você pode escrever código real desde o início. </p>
+          <hr />
+          <h1>Variedade de Linguagens e Tecnologias</h1>
+          <p> Temos uma ampla gama de linguagens e tecnologias. Desde linguagens populares como Python, JavaScript e Java até frameworks e bibliotecas.</p>
         </div>
-      </div>
       <div className={styles.kalix}>
+      <h2>Método de aprendizado</h2>
+        <div className={styles.cardContainer}>
+        <div className={styles.kalixCard}>
+            <h1>Foco</h1>
+            <p>Programação é um universo infinito, logo ter foco é essencial. Nós vamos direto ao ponto no que realmente importa para você dominar as ferramentas.</p>
+          </div>
+          <hr />
+          <div className={styles.kalixCard}>
+            <h1>Prática</h1>
+            <p>A programação é uma disciplina prática. São as horas de código que vão te preparar e dar a experiência necessária para tomar as melhores decisões no mundo real.</p>
+          </div>
+          <hr />
+          <div className={styles.kalixCard}>
+            <h1>Grupo</h1>
+            <p>A conexão entre Devs impulsiona seu repertório técnico, suas habilidades comportamentais e sua rede de contatos. Além de ajudar a assimilar e fixar todo aprendizado.</p>
+          </div>
+          </div>
       </div>
     </div>
   )
