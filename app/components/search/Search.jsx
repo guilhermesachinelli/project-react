85ea014dab5f0e430f23c0e8087b972b55097259
unknown <guilhermesachinelli.564@gamil.com>
2023-10-01 15:12:13 -0300

    add coment in search

--- app/components/search/Search.jsx
+++ app/components/search/Search.jsx
@@ -4,6 +4,9 @@ const Search = ({ search, setSearch }) => {
     return (
         <div><h2>Pesquisar :</h2>
         <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} placeholder="Digite para pesquisar."/>
+        {/*   e = evento, target = input, value = valor do input
+                com isso podemos  entender que o onchange serve para pegar o valor do input e setar no search
+                */}
         </div>
     )
 }
